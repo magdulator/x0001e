cf0938b705a87969da6ea81aedb80f490320b18d
magda <magdaa.a@hotmail.com>
2021-04-05 14:35:06 +0200

    comment

--- controllers/fibaro.js
+++ controllers/fibaro.js
@@ -3,6 +3,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
+//route to endpoint is defined in routes/routes.js and server.js
+
 exports.getAll = async (req, res) => {
     console.log(process.env.FIB_URL);
     var request = require('request'),
