dfcc61f0b25f774feb225949929d6436bd30ad76
Olofsson Robin <roblof-8@student.ltu.se>
2021-04-05 17:19:58 +0200

    getnode route

--- controllers/fibaro.js
+++ controllers/fibaro.js
@@ -5,6 +5,46 @@ dotenv.config();
 
 //route to endpoint is defined in routes/routes.js and server.js
 
+//Get info from a specific node
+exports.getNode = async (req,res) => {
+	var id = req.params.id;
+	console.log(process.env.FIB_URL);
+    var request = require('request'),
+    url = process.env.FIB_URL,
+    auth = process.env.FIB_AUTH;
+	await request({
+		url: url+"api/devices/"+id,
+		headers: {
+			"Authorization" : auth
+		}
+	},
+	function (error, response, body) {
+		res.send(body);
+	});
+};
+
+exports.getRoomNodes = async (req,res) => {
+	var roomID = req.params.roomID;
+    var request = require('request'),
+    url = process.env.FIB_URL,
+    auth = process.env.FIB_AUTH;
+	await request({
+		url: url+"api/devices/",
+		headers: {
+			"Authorization" : auth
+		}
+	},
+	function (error, response, body) {
+		/*body = JSON.parse(body);
+		for(var node in body){
+			//node = JSON.parse(node);
+			console.log(typeof node);
+		}*/
+		res.json(body);
+	});
+}
+
+
 exports.getAll = async (req, res) => {
     console.log(process.env.FIB_URL);
     var request = require('request'),
@@ -18,6 +58,5 @@ exports.getAll = async (req, res) => {
 	},
 	function (error, response, body) {
 		res.send(body);
-		
 	});
 };
--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,11 @@ const router = express.Router();
 //FIBARO ROUTES: http://localhost:5000/api/fibaro/getAll
 router.get('/fibaro/getAll', (fibaro.getAll));
 
+router.get('/fibaro/:id', (fibaro.getNode));
+
+router.get('/fibaro/nodes/:roomID', (fibaro.getRoomNodes));
+
+
 //WIDEFIND ROUTES
 
  module.exports = router;
