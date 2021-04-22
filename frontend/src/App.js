bb9e7adf71d2076f2f2581adb772d44678e441f7
magda <magdaa.a@hotmail.com>
2021-04-22 14:24:49 +0200

    navbar stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import LoginForm from "./components/login.component";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link, NavLink} from "react-router-dom";
 import auth from './services/auth';
 import {Images} from './components/images.component';
 import RegisterForm from './components/register.component';
+import {HouseDoor, PersonCircle} from 'react-bootstrap-icons';
 
 class App extends Component {
     constructor(props) {
@@ -24,53 +25,52 @@ class App extends Component {
       const {currentUser} = this.state;
       return (
       <div className="container">
-        
-          <div className="navbar-nav ml-auto">
           <header className="App-header">
         
           <BrowserRouter>
-          <nav className="navbar navbar-expand-lg fixed-bottom">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              {currentUser ? (
-              <li className="nav-item active">
+            
+              <nav className="navbar navbar-expand-lg fixed-bottom">
+                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                      {currentUser ? (
+                      <li className="nav-item">
 
-              <a href="/login" className="nav-link" onClick={this.logOut}>
-                    Logga ut {currentUser.role}
-              </a> </li>):(
-                <div>
-              <li className="nav-item active">
-              <Link to={"/images"} className="nav-link">
-                      Hem
-                  </Link>
-              </li>
-              
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                      regtstrer in
-                </Link>   
-              </li>
-              <li className="nav-item">
-                  <Link to={"/login"} className="nav-link">
-                      Logga in
-                  </Link>
-              </li>
-              <li className = "nav-item">
-                  <Link to={"/images"} className="nav-link">
-                      Images
-                  </Link>
-              </li></div>)}
-            </ul>
+                          <a href="/login" className="nav-link pl-3" onClick={this.logOut}>
+                              <PersonCircle size = "50" className ="ml-4"></PersonCircle><br></br>
+                                  <h4>LOGGA UT</h4>
+                          </a> 
+                      </li>):(
+                      <>
+                      <li className="nav-item ml-3">
+                          <NavLink to={"/images"} className="nav-link px-5" activeClassName="active-link">
+                              <HouseDoor size = "50"></HouseDoor><br></br>
+                              <h4>HEM</h4>        
+                          </NavLink>
+                      </li>
+                      <li className="nav-item ml-3">
+                          <NavLink to={"/login"} className="nav-link px-4" activeClassName="active-link">
+                              <PersonCircle size = "50" className ="ml-4"></PersonCircle><br></br>
+                              <h4>LOGGA IN</h4>
+                          </NavLink>
+                      </li>
+                     
+                      <li className="nav-item">
+                          <NavLink to={"/register"} className="nav-link px-5" activeClassName="active-link">
+                              regtstrer in
+                          </NavLink>   
+                      </li>
+                      
+                      </>
+                  )}
+              </ul>
           </nav>
           <Switch>
               <Route path = "/login" component={LoginForm}/>
               <Route path = "/register" component={RegisterForm}/>
               <Route path = "/images" component = {Images}/>
-
-            </Switch>
+          </Switch>
           </BrowserRouter>
           </header>
-        </div>    
-      </div>
+      </div> 
       )
     }
 }
