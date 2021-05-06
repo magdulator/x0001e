75885e8b09161c2f33eb5faabe91bac23f843346
magda <magdaa.a@hotmail.com>
2021-05-06 16:37:29 +0200

    end of daY

--- frontend/src/pages/system-overview.js
+++ frontend/src/pages/system-overview.js
@@ -4,11 +4,11 @@ const SystemOverview = () => {
     return(
         <div className = "d-flex justify-content-center flex-wrap overview text-center py-4">
             <h1 className="w-100">Klicka på ett system för att läsa mer om det</h1>
-            <div class="w-100"></div>
+            <div className="w-100"></div>
             <hr></hr>
 
-                <div class="row">
-                    <div class="col">
+                <div className="row">
+                    <div className="col">
                         <div className="card my-3 py-3">
                             <a className="card-block stretched-link text-decoration-none" href ="overview">
                             <div className = "card-body text-center">
@@ -16,7 +16,7 @@ const SystemOverview = () => {
                             </div></a>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card my-3 py-3">
                             <a className="card-block stretched-link text-decoration-none" href ="overview">
                             <div className = "card-body text-center">
@@ -24,8 +24,8 @@ const SystemOverview = () => {
                             </div></a>
                         </div>
                     </div>
-                    <div class="w-100"></div>
-                    <div class="col">
+                    <div className="w-100"></div>
+                    <div className="col">
                         <div className="card my-3 py-3">
                             <a className="card-block stretched-link text-decoration-none" href ="overview">
                             <div className = "card-body text-center">
@@ -33,7 +33,7 @@ const SystemOverview = () => {
                             </div></a>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card my-3 py-3">
                             <a className="card-block stretched-link text-decoration-none" href ="overview">
                             <div className = "card-body text-center">
@@ -41,9 +41,7 @@ const SystemOverview = () => {
                             </div></a>
                         </div>
                     </div>
-
-                </div>  
-                      
+                </div>             
         </div>
     )
 }
