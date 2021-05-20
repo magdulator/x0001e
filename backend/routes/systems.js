15511f15855d65f64895eb791f5ebfea9ff95034
magda <magdaa.a@hotmail.com>
2021-05-20 17:48:06 +0200

    error json

--- backend/routes/systems.js
+++ backend/routes/systems.js
@@ -8,10 +8,10 @@ router.patch('/update/:systemName', async(req, res) => {
     const updateInfo = req.body;
     try {
         const systemExists = await Systems.findOneAndUpdate(systemName, updateInfo);
-        if(!systemExists) return res.status(400).send("No system added with name " + req.params.systemName);
-        return res.send(req.params.systemName + " updated");
+        if(!systemExists) return res.status(400).json({message:"No system added with name " + req.params.systemName});
+        return res.status(200).json({message: req.params.systemName + " updated"});
     } catch(err) {
-        return res.status(400).send(err);
+        return res.status(400).json({message: err});
     }
 });
 
@@ -19,7 +19,7 @@ router.post('/create', async(req, res) => {
 
     //check if system already exists in db
     const systemExists = await Systems.findOne({ systemName : req.body.systemName});
-    if(systemExists) return res.status(400).send({message: "URL name already exists"});
+    if(systemExists) return res.status(400).json({message: "URL name already exists"});
 
     const titleExists = await Systems.findOne({ title : req.body.title});
     if(titleExists) return res.status(400).json({message: "Title already exists"});
@@ -33,19 +33,21 @@ router.post('/create', async(req, res) => {
     });
     try {
         const savedSystem = await system.save();
-        return res.send(savedSystem.systemName + " registered");
+        return res.status(200).json({message: savedSystem.systemName + " registered"});
     } catch(err) {
-        return res.status(401).send(err);
+        return res.status(401).json({message: "Could not save"});
     }
 });
 
 router.get('/:systemName', async(req,res) => {
     try {
         const systemExists = await Systems.findOne({ systemName : req.params.systemName});
-        return res.send(systemExists);
+        if(!systemExists) return res.status(400).json({message: "System not found"});
+
+        return res.status(200).json({message: systemExists});
 
     } catch(err) {
-        return res.status(400).send(err);
+        return res.status(400).json({message: err});
     }
 
 });
@@ -53,9 +55,9 @@ router.get('/:systemName', async(req,res) => {
 router.get('/', async(req,res) => {
     try {
         const systems = await Systems.find({}, {title: 1, systemName:1, _id:0});
-        return res.send(systems);
+        return res.status(200).json({message: systems});
     } catch(err) {
-        return res.status(400).send(err);
+        return res.status(400).json({message: err});
     }
 });
 
