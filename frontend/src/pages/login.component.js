ca9cc6235a2695cf3871b58eb3b84eba0aa9cba2
magda <magdaa.a@hotmail.com>
2021-05-20 16:26:55 +0200

    login view is nice now

--- frontend/src/pages/login.component.js
+++ frontend/src/pages/login.component.js
@@ -2,70 +2,76 @@
     import auth from '../services/auth';
     import { Formik,ErrorMessage} from 'formik';
     import { useHistory } from "react-router-dom";
-    
-
     import * as Yup from 'yup';
     import {PersonCircle} from 'react-bootstrap-icons';
 
 
    const LoginForm = () => {
-
         let history = useHistory();
-
         return(
             <Formik 
                 initialValues={{
                     email: '',
-                    password: ''
+                    password: '',
+                    status: false
                 }}
                 validationSchema ={Yup.object().shape( {
-                    email: Yup.string().email('email invalid').required('Email is required'),
-                    password: Yup.string().min(3, 'Password needs to be at least 6 characters').required('Password is required')
+                    email: Yup.string().email(<div className = "alert alert-danger">Email invalid</div>).required(<div className = "alert alert-danger">Email is required</div>),
+                    password: Yup.string().min(3, <div className = "alert alert-danger">Password needs to be at least 6 characters</div>).required(<div className = "alert alert-danger">Password is required</div>)
                 })}
                 onSubmit={ async values  => {
 
                     if(await auth.login(values.email, values.password) === false) {
                         alert('Wrong password or email')
                     }
-                    else history.push('/home')
-                    
+                    else {
+                        values.status = true;
+                        history.push('/home')
+                    }
                 }}
                 validator={() => ({})}>
                     { props => (
-                    <div className = "d-flex justify-content-center">
-
-                        <div className="card mx-auto">
-                            <div className="card-header text-center">
-                                <h4>Logga in</h4>
-                            </div>
-                            <div className = "card-body text-center">
-                                <PersonCircle size = "80"></PersonCircle>
-
-                                <form onSubmit =  {props.handleSubmit} >
-
-                                    <div className="form-group input-group-lg">
-                                        <label htmlFor="email" className= " col-form-label"></label>
-                                        <input name="email" type="email" placeholder="Email adress" className = "form-control" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur}  />
-                                        <ErrorMessage name="email" />
+                        <div className = "main">
+                            <div className = "login d-flex justify-content-center">
+                                <div className="card w-100">
+                                    <div className="card-header text-center">
+                                        <h4>Logga in</h4>
                                     </div>
-                                    <div className="form-group input-group-lg">
-                                        <label htmlFor="password" className= "col-form-label" ></label>
-                                        <input name="password" type="password" placeholder="Lösenord" className = "form-control" value={props.values.password} onChange={props.handleChange} onBlur={props.handleBlur}/>
-                                        <ErrorMessage name="password" /> 
-                                    </div>                                 
-                                    <div className = "form-group">
-                                        <button
-                                        className = "btn btn-primary btn-lg mt-3 py-3"
-                                        type="submit"
-                                        disabled={props.isSubmitting}> <h3>LOGGA IN</h3>
-                                        </button>
+                                    <div className = "card-body text-center">
+                                        <PersonCircle size = "80"></PersonCircle>
+
+                                        <form onSubmit =  {props.handleSubmit} >
+
+                                            <h5 className = "py-2">Email adress:</h5>
+                                            <div className="input-group input-group-lg">
+                                                <input name="email" type="email" className = "form-control" value={props.values.email || ''} onChange={props.handleChange} onBlur={props.handleBlur}  />
+                                            </div>
+                                            <ErrorMessage name="email"/>
+
+                                            <h5 className = "py-2">Lösenord:</h5>
+                                            <div className="input-group input-group-lg">
+                                                <input name="password" type="password" className = "form-control" value={props.values.password || ''} onChange={props.handleChange} onBlur={props.handleBlur}/>
+                                            </div>
+                                            <ErrorMessage name="password" /> 
+
+                                            <div className = "form-group">
+                                                <button
+                                                className = "btn btn-primary btn-lg btn-block my-3 py-3 px-2"
+                                                type="submit"
+                                                disabled={props.isSubmitting}> <h4>LOGGA IN</h4>
+                                                </button>
+                                            </div> 
+                                            {props.values.status  && (
+                                                <div className = "alert alert-success" role="alert">
+                                                    <h5>Lyckad inloggning</h5>
+                                                </div>
+                                            )}
+                                        </form>
                                     </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )}
-            </Formik>
-            
+                    )}
+            </Formik> 
         )}
     export{LoginForm}
