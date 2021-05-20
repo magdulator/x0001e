4ee04b9d02a41e5cd545ced59948d4a60e1063c1
magda <magdaa.a@hotmail.com>
2021-05-20 16:00:15 +0200

    register errors

--- frontend/src/pages/register.component.js
+++ frontend/src/pages/register.component.js
@@ -5,8 +5,6 @@ import * as Yup from 'yup';
 import {PersonCircle} from 'react-bootstrap-icons';
 
 export default class RegisterForm extends Component {
-
-
     render(){
     return(
         <Formik 
@@ -16,8 +14,8 @@ export default class RegisterForm extends Component {
                 picked: ''
             }}
             validationSchema ={Yup.object().shape( {
-                email: Yup.string().email('email invalid').required('Email is required'),
-                password: Yup.string().min(3, 'Password needs to be at least 6 characters').required('Password is required')
+                email: Yup.string().email(<div className = "alert alert-danger">Email invalid</div>).required(<div className = "alert alert-danger">Email is required</div>),
+                password: Yup.string().min(3, <div className = "alert alert-danger">Password needs to be at least 6 characters</div>).required(<div className = "alert alert-danger">Password is required</div>)
             })}
             onSubmit={ async values  => {
                 await auth.register("hej", values.email, values.password, values.picked)
@@ -25,53 +23,53 @@ export default class RegisterForm extends Component {
             }}
             validator={() => ({})}>
                 { props => (
-                    <div className = "container py-5">
-                        <div className="card mx-auto">
-                            <div className="card-header text-center">
-                                <h4>Registrera en ny användare</h4>
+                    <div className="main">
+                        <div className = "register py-5">
+                            <div className="card mx-auto">
+                                <div className="card-header text-center">
+                                    <h4>Registrera en ny användare</h4>
                                 </div>
                                 <div className = "card-body text-center">
                                 <PersonCircle size = "50"></PersonCircle>
-                    <form onSubmit = {props.handleSubmit} >
+                                <form onSubmit = {props.handleSubmit} >
+                                    <h5 className = "py-2">Email adress:</h5>
+                                    <div className="input-group input-group-lg">
+                                        <input name="email" type="email" className = "form-control" value={props.values.email || ''} onChange={props.handleChange} onBlur={props.handleBlur}  />
+                                    </div>
+                                    <ErrorMessage name="email"/>
+                                    <h5 className = "py-2">Lösenord:</h5>
+                                    <div className="input-group input-group-lg">
+                                        <input name="password" type="password" className = "form-control" value={props.values.password || ''} onChange={props.handleChange} onBlur={props.handleBlur}/>
+                                    </div>
+                                    <ErrorMessage name="password" /> 
 
-                    <div className="form-group input-group-lg">
-                        <label htmlFor="email" className= "">Email</label>
-                            <input name="email" type="email" className = "form-control" value={props.values.email} onChange={props.handleChange} onBlur={props.handleBlur}  />
-                            <ErrorMessage name="email" className="invalid-feedback"/>
+                                    <div className = "radio-container">  
+                                    <div className = "form-check py-2">
+                                        <input className = 'form-check-input' type="radio" name="picked" value="admin" onChange={props.handleChange} id = "1"/>
+                                        <label className ="check-label" htmlFor="1">
+                                            Admin
+                                        </label>                      
+                                    </div>
+                                    <div className = "form-check py-2">
+                                        <input className='form-check-input' type="radio" name="picked" value="power-user" onChange={props.handleChange} id ="2"/>
+                                        <label className ="check-label" htmlFor="2">
+                                            Power-user
+                                        </label> 
+                                    </div>
+                                </div>
+                                <div className = "form-group">
+                                    <button
+                                    className = "btn btn-primary btn-lg mt-3 py-2"
+                                    type="submit"
+                                    disabled={props.isSubmitting}> <h4>REGISTRERA ANVÄNDARE</h4>
+                                    </button>
+                                </div> 
+                            </form>
+                        </div>
                     </div>
-                    <div className="form-group input-group-lg ">
-                        <label htmlFor="password" className= "" >Password</label>
-                            <input name="password" type="password" className = "form-control" value={props.values.password} onChange={props.handleChange} onBlur={props.handleBlur}/>
-                            <ErrorMessage name="password" /> 
-                            
-                    </div>  
-                    <div className = "form-check">
-                        <input className = 'form-check-input' type="radio" name="picked" value="admin" onChange={props.handleChange} id = "1"/>
-                        <label className ="form-check-label" htmlFor="1">
-                            Admin
-                        </label>                      
-                    </div>
-                    <div className = "form-check">
-                        <input className='form-check-input' type="radio" name="picked" value="power-user" onChange={props.handleChange} id ="2"/>
-                        <label className ="form-check-label" htmlFor="2">
-                            Power-user
-                        </label> 
-                    </div>
-                    <div className = "form-group">
-                        <button
-                        className = "btn btn-primary btn-lg mt-3 py-2"
-                        type="submit"
-                        disabled={props.isSubmitting}> <h4>REGISTRERA ANVÄNDARE</h4>
-                        </button>
-                    </div> 
-
-                </form>
                 </div>
-                </div>
-                </div>
-
+            </div>
             )}
-        </Formik>
-        
+        </Formik> 
     )}
 }
